275575e49000917fe9e1d494864c10e672cf4bb0
prakasiwi <muhammadgalih282@gmail.com>
2022-08-27 11:07:03 +0700

    update func and module

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/prakasiwi/belajar-golang
+module github.com/prakasiwi/belajar-golang/v2
 
 go 1.17
 
--- say_hello.go
+++ say_hello.go
@@ -1,4 +1,4 @@
 package go_say_hello	
-func SayHello() string {
-	return "Hello Broku"
+func SayHello(name string) string {
+	return "Hello Broku" + name
 }
